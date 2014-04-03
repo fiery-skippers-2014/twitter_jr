d2f15c2354fc7cb907e3986755d41a6005e6fa02
Apprentice <apprentice@dbc23.local>
2014-04-03 11:50:26 -0700

    user signup sign in controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,12 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/users/:id/profiles' do
+redirect '/users/:id/profiles'
+end
+
+get '/users/:id/profiles' do
+  erb :user_profile
+end
+
